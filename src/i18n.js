e4bce972a69e0ac9e79702fe9cd6968d5f11a2c5
giorgos apostolidis <apostolidesg1@gmail.com>
2025-03-08 11:49:29 +0200

    locales and icons install

---
+++ src/i18n.js
@@ -0,0 +1,16 @@
+import { createI18n } from "vue-i18n";
+import en from "./locales/en.json";
+import el from "./locales/el.json";
+
+const messages = {
+  en,
+  el,
+};
+
+const i18n = createI18n({
+  locale: "en",
+  fallbackLocale: "en",
+  messages,
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 import router from "./router/index.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import i18n from "./i18n";
 
 const app = createApp(App);
 app.use(router);
+app.use(i18n);
 app.mount("#app");
