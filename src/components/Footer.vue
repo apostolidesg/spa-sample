9b94053d4ab3c24c990dde9ae302037dd6e0adb4
giorgos apostolidis <apostolidesg1@gmail.com>
2025-03-09 22:59:16 +0200

    implemented footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>Footer</div>
+  <div class="footer" :class="themeClass">
+    <div class="footer-text">&copy; {{ $t("footer.copyrights") }}</div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "white",
+      validator: (value) => ["white", "dark-green"].includes(value),
+    },
+  },
+  computed: {
+    themeClass() {
+      return `footer-${this.theme}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../assets/scss/variables" as *;
+
+.footer {
+  font-size: 0.8em;
+  padding: 2em;
+  &-text {
+    display: flex;
+    justify-content: center;
+    color: $background-color-primary;
+  }
+  &-white {
+    background-color: $background-color-secondary;
+  }
+}
+</style>
