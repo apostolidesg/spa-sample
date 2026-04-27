50b4f37c3719b395da2b2661d0168656394168f5
apostolidesg <apostolidesg1@gmail.com>
2026-04-27 15:43:11 +0300

    remove footer component

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,14 @@
   <router-view v-slot="{ Component }">
     <component :is="Component" :key="$route.path" />
   </router-view>
-  <Footer v-if="hasGDPR" />
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Navbar,
-    Footer,
-  },
-  data() {
-    return {
-      hasGDPR: false,
-    };
   },
 };
 </script>
